6ac10f8f7d116056eb8e57555aee11e6bbc9a559
Tim Oxley <secoif@gmail.com>
2015-08-09 01:01:27 -0500

    Fix test because can't pass 3.0-next to npm info dist-tags on cli.

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var test = require('tape')
 var exec = require('child_process').exec
 
 test('uses npm@3.0-next', function(t) {
-  exec('npm info npm dist-tags.3.0-next', function(err, expected) {
-    expected = expected.trim()
+  exec('npm info npm dist-tags --json', function(err, jsonText) {
+    var expected = JSON.parse(jsonText)['3.0-next']
     exec('./bin/index.js -v', function(err, actual) {
       actual = actual.trim()
       t.equal(actual, expected, 'uses 3.0-next version')
