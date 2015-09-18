883247f06f85e16f8f3e81fc95174419518e0e41
Tim Oxley <secoif@gmail.com>
2015-09-18 21:22:04 +0800

    Upgrade to npm@3.3.3

--- test.js
+++ test.js
@@ -3,12 +3,12 @@
 var test = require('tape')
 var exec = require('child_process').exec
 
-test('uses npm@3.0-next', function(t) {
-  exec('npm info npm dist-tags --json', function(err, jsonText) {
-    var expected = JSON.parse(jsonText)['3.0-next']
+test('uses npm@^3.0.0', function(t) {
+  exec('npm info npm@^3.0.0 version --json | tail -n 1 | cut -d \' \' -f 2', function(err, version) {
+    var expected = JSON.parse(version)
     exec('./bin/index.js -v', function(err, actual) {
       actual = actual.trim()
-      t.equal(actual, expected, 'uses 3.0-next version')
+      t.equal(actual, expected, 'uses latest version of npm v3')
       t.end()
     })
   })
