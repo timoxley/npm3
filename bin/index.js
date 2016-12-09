c57b52071ac486df123857edcdaf604c81b11823
Tim Oxley <secoif@gmail.com>
2016-12-09 15:04:13 +0800

    Ensure exit code matches proxied npm.
    Closes #3, #4.

--- bin/index.js
+++ bin/index.js
@@ -4,3 +4,6 @@ var spawn = require('child_process').spawn
 var npm = require.resolve('npm/cli')
 
 spawn(process.execPath, [npm].concat(process.argv.slice(2)), {stdio: 'inherit'})
+.on('exit', function (code) {
+  process.exit(code)
+})
--- test.js
+++ test.js
@@ -1,14 +1,41 @@
-"use strict"
+'use strict'
 
 var test = require('tape')
 var exec = require('child_process').exec
 
-test('uses npm@^3.0.0', function(t) {
-  exec('npm info npm@^3.0.0 version --json', function(err, version) {
+test('uses npm@^3.0.0', function (t) {
+  exec('npm info npm@^3.0.0 version --json', function (err, version) {
+    t.ifError(err)
+    t.ok(version, 'has version')
     var expected = JSON.parse(version).pop()
-    exec('./bin/index.js -v', function(err, actual) {
+    exec('./bin/index.js -v', function (err, actual) {
+      t.ifError(err)
       actual = actual.trim()
       t.equal(actual, expected, 'uses latest version of npm v3')
+      t.end()
+    })
+  })
+})
+
+test('exit error codes match', function (t) {
+  exec('npm ls asdasd', function (expectedErr) {
+    t.ok(expectedErr)
+    t.ok(expectedErr.code, 'expected has non-zero err code')
+    exec('./bin/index.js ls asdasd', function (actualErr) {
+      t.equal(expectedErr.code, actualErr.code, 'expected error code matches actual code')
+      t.end()
+    })
+  })
+})
+
+test('exec matches', function (t) {
+  exec('npm ls npm --json', function (err, expected) {
+    t.ifError(err)
+    expected = JSON.parse(expected)
+    exec('./bin/index.js ls npm --json', function (err, actual) {
+      t.ifError(err)
+      actual = JSON.parse(actual)
+      t.deepEqual(expected, actual, 'output matches npm3')
       t.end()
     })
   })
