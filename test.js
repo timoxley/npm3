6a89030a834bd8f8af1e477944e9b8b765efd76a
Tim Oxley <secoif@gmail.com>
2016-12-09 15:02:40 +0800

    Fix tests. Latest npm updates changed output somehow.

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var test = require('tape')
 var exec = require('child_process').exec
 
 test('uses npm@^3.0.0', function(t) {
-  exec('npm info npm@^3.0.0 version --json | tail -n 1 | cut -d \' \' -f 2', function(err, version) {
-    var expected = JSON.parse(version)
+  exec('npm info npm@^3.0.0 version --json', function(err, version) {
+    var expected = JSON.parse(version).pop()
     exec('./bin/index.js -v', function(err, actual) {
       actual = actual.trim()
       t.equal(actual, expected, 'uses latest version of npm v3')
